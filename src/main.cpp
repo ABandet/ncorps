1b2e5fbc709b868795746104b1bdb7d727e4d724
Alexis BANDET <alexis.bandet@inria.fr>
2026-09-19 23:38:21 +0200

    adding Kokkos as dependency

--- src/main.cpp
+++ src/main.cpp
@@ -14,19 +14,17 @@ using namespace ncorps;
 
 int main(void) {
     Bodies b(N);
-    ForceModel f{};
-    EulerExplicit e{};
 
     std::vector<double> fx(N), fy(N), fz(N);
 
     for (int t = 0; t < NB_ITER; t++) {
         // update force for n
         for (size_t i = 0; i < N; i++) {
-            f.step(b, i, DT, fx[i], fy[i], fz[i]);
+            ForceModel::step(b, i, DT, fx[i], fy[i], fz[i]);
         }
         // update position for n+1
         for (size_t i = 0; i < N; i++) {
-            e.step(b, i, DT, fx[i], fy[i], fz[i]);
+            EulerExplicit::step(b, i, DT, fx[i], fy[i], fz[i]);
         }
     }
 
