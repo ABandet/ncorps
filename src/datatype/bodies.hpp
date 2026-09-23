824cb9e77cd45313a94b24407d72a391e39f9746
Alexis BANDET <alexis.bandet@inria.fr>
2026-09-23 18:53:08 +0200

    adding omp and random body generation

--- src/datatype/bodies.hpp
+++ src/datatype/bodies.hpp
@@ -1,6 +1,10 @@
 #ifndef BODIES_H_
 #define BODIES_H_
 
+#include <cmath>
+#include <cstddef>
+#include <iostream>
+#include <random>
 #include <vector>
 
 namespace ncorps {
@@ -21,6 +25,33 @@ class Bodies {
         this->m_vz = std::vector<double>(n, 0);
 
         this->m_m = std::vector<double>(n, 1);
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<double> dis(1.0, 1000.0);
+        for (size_t i = 0; i < m_n; i++) {
+            m_rx[i] = dis(gen);
+            m_ry[i] = dis(gen);
+            m_rz[i] = dis(gen);
+        }
+    }
+
+    double get_body_energy(size_t i) const {
+        const double speed = std::sqrt(
+            std::pow(m_vx[i], 2) * std::pow(m_vy[i], 2) * std::pow(m_vz[i], 2));
+
+        const double energy = 0.5 * 1 * std::pow(speed, 2);
+        return energy;
+    }
+
+    // compute sum of cinetic energies in the bodies collection
+    double get_system_energy() const {
+        double sys_energy = 0;
+        for (size_t i = 0; i < m_n; i++) {
+            sys_energy += get_body_energy(i);
+        }
+
+        return sys_energy;
     }
 
     size_t m_n{0};
--- src/main.cpp
+++ src/main.cpp
@@ -30,25 +30,69 @@ void run_seq(Bodies &b, double dt, int nb_iter) {
     }
 }
 
+template <ForceModelC FM, TimeIntegrator TI>
+void run_omp(Bodies &b, double dt, int nb_iter) {
+    std::vector<double> fx(N), fy(N), fz(N);
+#pragma omp parallel for
+    for (int t = 0; t < NB_ITER; t++) {
+        // update force for n
+        for (size_t i = 0; i < N; i++) {
+            FM::step(b, i, DT, fx[i], fy[i], fz[i]);
+        }
+        // update position for n+1
+#pragma omp parallel for
+        for (size_t i = 0; i < N; i++) {
+            TI::step(b, i, DT, fx[i], fy[i], fz[i]);
+        }
+    }
+}
+
 int main(void) {
 
     std::cout << "Running simulation on " << NB_ITER << " time iteration"
               << std::endl;
-
     std::cout << "With " << N << " bodies" << std::endl;
 
     auto start = std::chrono::high_resolution_clock::now();
 
+    // compute for seq model
     Bodies b(N);
-
+    const auto init_b_nrj = b.get_system_energy();
+    auto start_seq = std::chrono::high_resolution_clock::now();
     run_seq<ForceModel, EulerExplicit>(b, DT, NB_ITER);
+    auto end_seq = std::chrono::high_resolution_clock::now();
+    const auto end_b_nrj = b.get_system_energy();
 
+    // compute for omp model
+    Bodies b_omp(N);
+    const auto init_bomp_nrj = b.get_system_energy();
+    auto start_omp = std::chrono::high_resolution_clock::now();
+    run_omp<ForceModel, EulerExplicit>(b_omp, DT, NB_ITER);
+    auto end_omp = std::chrono::high_resolution_clock::now();
+    const auto end_bomp_nrj = b.get_system_energy();
+
+    // compute execution times
     auto end = std::chrono::high_resolution_clock::now();
-
     auto duration =
         std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
+    auto duration_seq = std::chrono::duration_cast<std::chrono::milliseconds>(
+        end_seq - start_seq);
+    auto duration_omp = std::chrono::duration_cast<std::chrono::milliseconds>(
+        end_omp - start_omp);
+
+    // compute energy difference accros models
+    const auto nrj_diff_seq = end_b_nrj - init_b_nrj;
+    const auto nrj_diff_omp = end_bomp_nrj - init_bomp_nrj;
+
+    // simple prompt
     std::cout << "Execution Time: " << duration.count() << " ms" << std::endl;
+    std::cout << "SEQ Time: " << duration_seq.count() << " ms" << std::endl;
+    std::cout << "OMP Time: " << duration_omp.count() << " ms" << std::endl;
+    std::cout << "SEQ energy difference: " << nrj_diff_seq << " Joules"
+              << std::endl;
+    std::cout << "OMP energy difference: " << nrj_diff_omp << " Joules"
+              << std::endl;
 
     return 0;
 }
